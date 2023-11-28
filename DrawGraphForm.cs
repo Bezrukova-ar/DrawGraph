862050ae1f3ce7b284b508008fdd19f192f26dbb
USER <USER@DESKTOP-8LRHPT1>
2023-11-28 09:29:26 +0400

    Продолжение рефакторинга

--- DrawGraphForm.cs
+++ DrawGraphForm.cs
@@ -56,68 +56,11 @@ namespace DrawGraph
             graph.edges.Add(new Graph.Edge(5, 6, 2));
 
             // Построение графа
-            DrawGraph();
+            graph.DrawGraph(sheet);
 
             IndividualTaskForm individualTaskForm = new IndividualTaskForm();
             individualTaskForm.Show();
-        }
-        //Метод для отрисовки индивидуального графа
-        private void DrawGraph()
-        {
-            using (Graphics g = sheet.CreateGraphics())
-            {
-                // Очистка pictureBox
-                g.Clear(Color.White);
-                Font weightFont = new Font(Font.FontFamily, 12f);
-                Font boldFont = new Font(Font, FontStyle.Bold);
-
-                //Рисование ребер
-                foreach (var edge in graph.edges)
-                {
-
-                    Point start = graph.vertices.Find(v => v.Number == edge.StartVertex).Position;
-                    Point end = graph.vertices.Find(v => v.Number == edge.EndVertex).Position;
-
-                    // Рисование толстых линий
-                    using (Pen edgePen = new Pen(Color.Black, 2f))
-                    {
-                        g.DrawLine(edgePen, start, end);
-                    }
-
-                    // Вычисление позиции для веса в первой трети ребра
-                    float weightPositionX = start.X + (end.X - start.X) / 3;
-                    float weightPositionY = start.Y + (end.Y - start.Y) / 3;
-
-                    // Вывод веса ребра в первой трети
-                    g.DrawString(edge.Weight.ToString(), weightFont, Brushes.DarkRed, weightPositionX, weightPositionY);
-                }
-
-                // Рисование вершин
-                foreach (var vertex in graph.vertices)
-                {
-                    float textWidth = g.MeasureString(vertex.Number.ToString(), Font).Width;
-                    float textHeight = g.MeasureString(vertex.Number.ToString(), Font).Height;
-
-                    // Учитываем радиус круга при расчете координат центра
-                    float x = vertex.Position.X - 10;  // Учитываем половину диаметра (20/2)
-                    float y = vertex.Position.Y - 10;  // Учитываем половину диаметра (20/2)
-
-                    // Рисование контура круга
-                    using (Pen circlePen = new Pen(Color.Black, 3f))
-                    {
-                        g.DrawEllipse(circlePen, x, y, 20, 20);
-                    }
-                    // Закрашивание круга
-                    g.FillEllipse(Brushes.LavenderBlush, x, y, 20, 20);
-
-                    // Рисование номера вершины в центре
-                    float textX = x - textWidth / 2 + 10;  // Учитываем половину ширины текста
-                    float textY = y - textHeight / 2 + 10; // Учитываем половину высоты текста
-
-                    g.DrawString(vertex.Number.ToString(), Font, Brushes.Black, textX, textY);
-                }
-            }
-        }
+        }      
 
         //обработчик щелчка мыши по pictureBox   
         private void sheet_MouseClick(object sender, MouseEventArgs e)
@@ -262,7 +205,8 @@ namespace DrawGraph
         {
             // Создайте объект Graphics для рисования на PictureBox
             Graphics g = e.Graphics;
-            Font weightFont = new Font(Font.FontFamily, 12f);
+            Font weightFont = new Font("Arial", 12f);
+            Font boldFont = new Font(weightFont, FontStyle.Bold);
             //Рисование ребер
             foreach (var edge in graph.edges)
             {
--- Graph.cs
+++ Graph.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DrawGraph
 {
@@ -39,5 +40,63 @@ namespace DrawGraph
                 Weight = weight;
             }
         }
+        //Метод для отрисовки индивидуального графа
+        public void DrawGraph(PictureBox sheet)
+        {
+            using (Graphics g = sheet.CreateGraphics())
+            {
+                // Очистка pictureBox
+                g.Clear(Color.White);
+                Font weightFont = new Font("Arial", 12f);
+                Font boldFont = new Font(weightFont, FontStyle.Bold);
+
+                //Рисование ребер
+                foreach (var edge in edges)
+                {
+
+                    Point start = vertices.Find(v => v.Number == edge.StartVertex).Position;
+                    Point end = vertices.Find(v => v.Number == edge.EndVertex).Position;
+
+                    // Рисование толстых линий
+                    using (Pen edgePen = new Pen(Color.Black, 2f))
+                    {
+                        g.DrawLine(edgePen, start, end);
+                    }
+
+                    // Вычисление позиции для веса в первой трети ребра
+                    float weightPositionX = start.X + (end.X - start.X) / 3;
+                    float weightPositionY = start.Y + (end.Y - start.Y) / 3;
+
+                    // Вывод веса ребра в первой трети
+                    g.DrawString(edge.Weight.ToString(), weightFont, Brushes.DarkRed, weightPositionX, weightPositionY);
+                }
+
+                // Рисование вершин
+                foreach (var vertex in vertices)
+                {
+                    float textWidth = g.MeasureString(vertex.Number.ToString(), weightFont).Width;
+                    float textHeight = g.MeasureString(vertex.Number.ToString(), weightFont).Height;
+
+                    // Учитываем радиус круга при расчете координат центра
+                    float x = vertex.Position.X - 10;  // Учитываем половину диаметра (20/2)
+                    float y = vertex.Position.Y - 10;  // Учитываем половину диаметра (20/2)
+
+                    // Рисование контура круга
+                    using (Pen circlePen = new Pen(Color.Black, 3f))
+                    {
+                        g.DrawEllipse(circlePen, x, y, 20, 20);
+                    }
+                    // Закрашивание круга
+                    g.FillEllipse(Brushes.LavenderBlush, x, y, 20, 20);
+
+                    // Рисование номера вершины в центре
+                    float textX = x - textWidth / 2 + 10;  // Учитываем половину ширины текста
+                    float textY = y - textHeight / 2 + 10; // Учитываем половину высоты текста
+
+                    g.DrawString(vertex.Number.ToString(), weightFont, Brushes.Black, textX, textY);
+                }
+            }
+        }
     }
+
 }
